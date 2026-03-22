e2086eb767c3f4bdb460fc987786c5c84fa3afdc
Devi varaprasad Dulla <devivaraprasaddulla@gmail.com>
2026-03-22 12:02:14 +0530

    changed vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+{
+  "routes": [
+    {
+      "src": "^/assets/(.*)",
+      "dest": "/assets/$1"
+    },
+    {
+      "src": "^(?!/assets/).*$",
+      "dest": "/index.html"
+    }
+  ]
+}
